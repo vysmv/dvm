d3f0421267410e6c03dd9ef2ab2f6434d1f6a71a
vysmv <vysmv.contact@gmail.com>
2025-12-19 15:43:59 +0200

    added v2

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/vysmv/dvm/v2
+
+go 1.25.1
---
+++ v2/utils/utils.go
@@ -0,0 +1,15 @@
+package utils
+
+import "fmt"
+
+func HelloNewAPI() {
+    fmt.Println("Hello from utils!")
+}
+
+func NewFuncV0() {
+    fmt.Println("Hello from utils!")
+}
+
+func NewFuncV1() {
+    fmt.Println("Hello from utils!")
+}
